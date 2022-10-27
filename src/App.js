bbdc2ea08d1afd820b7e60af1be19fab5b1d1713
Brandon <106634008+brandon31@users.noreply.github.com>
2022-10-27 02:30:04 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '4866f46546mshf7c7a9f018e57c8p1ac215jsnd22ee905696d',
+        'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://open-weather13.p.rapidapi.com/city/landon', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello</h1>
+    <span class="material-symbols-outlined">
+      search
+    </span>
+    <span class="material-symbols-outlined">
+      cloud
+    </span>
+    <span class="material-symbols-outlined">
+    sunny
+    </span>
+    <span class="material-symbols-outlined">
+    dark_mode
+    </span>
+    <span class="material-symbols-outlined">
+    humidity_high
+    </span>
+    <span class="material-symbols-outlined">
+    compress
+    </span>
+    <span class="material-symbols-outlined">
+    arrow_upward
+    </span>
+    <span class="material-symbols-outlined">
+    arrow_downward
+    </span>
+    <span class="material-symbols-outlined">
+    trending_up
+    </span>
+    </>
   );
 }
 
